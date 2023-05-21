73823a832de1c2ec2396c377d75b8c7713dcbbc0
Pterodaktilitsa <pterodaktilitsa@gmail.com>
2023-05-21 22:54:09 +0300

    second version without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,34 @@
 import logo from './logo.svg'; //декларативный
 import './App.css';
+import React from 'react';
 
 const newYear = new Date(); //императивный
 export const App = () => {
-	return (
-		//декларативный
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p> {newYear.getFullYear()} </p>
-			</header>
-		</div>
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('p', null, newYear.getFullYear()),
+		),
 	);
 };
