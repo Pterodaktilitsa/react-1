00d5f9faeaf7d1546afc0d1b63bec0d341c47e96
Pterodaktilitsa <pterodaktilitsa@gmail.com>
2023-06-01 12:55:36 +0300

    Calculator was added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,43 @@
-import { MyComponent } from './MyComponent';
+import { useState } from 'react';
 import styles from './App.module.css';
 
 export const App = () => {
+	const [calc, setCalc] = useState('');
+	const [result, setResult] = useState('');
+
+	const ops = ['+', '-'];
+	const updateCalc = (value) => {
+		setCalc(calc + value);
+	};
+	const createDigits = () => {
+		const digits = [];
+		for (let i = 1; i < 10; i++) {
+			digits.push(
+				<button onClick={() => updateCalc(i.toString())} key={i}>
+					{i}
+				</button>,
+			);
+		}
+		return digits;
+	};
 	return (
 		<div className={styles.app}>
-			<header className={styles.header}>
-				<MyComponent />
-			</header>
+			<div className="calculator">
+				<div className="display">
+					{result ? <span>(0)</span> : ''} {calc || '0'}
+				</div>
+
+				<div className="operators">
+					<button onClick={() => updateCalc('+')}>+</button>
+					<button onClick={() => updateCalc('-')}>-</button>
+					<button onClick={() => updateCalc('=')}>=</button>
+					<button>C</button>
+				</div>
+				<div className="digits">
+					{createDigits()}
+					<button onClick={() => updateCalc('0')}>0</button>
+				</div>
+			</div>
 		</div>
 	);
 };
